21c49185e3b918669558c917d88914a49b909f32
css <sishuochen@qq.com>
2019-05-18 13:29:27 +0800

    add readme

---
+++ src/main/java/arxiv/User.java
@@ -0,0 +1,12 @@
+package arxiv;
+import com.avos.avoscloud.*; 
+
+public class User extends AVUser{
+	
+	public static void main(String args[]) {
+		AVOSCloud.initialize("m0rQBplH1SM5GAYFvt22VI0z-gzGzoHsz","MS8SRy9i4rsKkk0j8HtDJm6x","EAElMEaXzhmulzrx0ddcey0Q");
+        System.out.println( "Hello World! PKU£¡"  );
+        AVOSCloud.setDebugLogEnabled(true);
+	}
+}
+
